8433220a40bc4dfb2d997e9e23e862e7299893e2
es <sadiksanhaji@gmail.com>
2020-12-26 22:01:50 +0100

    cashing the dom

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 
+let random = document.getElementById('random');
+
+let black = document.getElementById('black')
+let eraser = document.getElementById('eraser')
+let incrementD = document.getElementById('incdark')
+let colorPicker= document.getElementById('pick')
+let drowingBoard = document.querySelector(".dbox")
+console.log(drowingBoard)
+
 function getRandomColor() {
 
     colors = new Array("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F");
