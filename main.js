8e5e4469b04eb84d6aef02827dc4a4396c7895b3
es <sadiksanhaji@gmail.com>
2021-01-05 01:55:54 +0100

    ""

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ let drowingBoard = document.querySelector(".dbox");
 
 let myRange = document.getElementById('myRange');
 
+let color = '#000000';
 
 //geting a value from the range slider
 let rangeValue = document.querySelector('.rangeValue');
@@ -26,18 +27,20 @@ function creatGridCells() {
     if (gridNumber == 16) {
         rangeValue.textContent = 'Max'
     };
-    for (i = 0; i < (gridNumber * gridNumber); i++) {
+    for (i = 0; i < (gridNumber ** 4); i++) {
         let gridCells = document.createElement('div');
         drowingBoard.appendChild(gridCells).classList.add('gridCells');
-        drowingBoard.style.cssText = ` grid-template-columns: repeat( ${gridNumber * gridNumber}, 1fr);
-                                       grid-template-rows: repeat( ${gridNumber * gridNumber}, 1fr);`
+        drowingBoard.style.cssText = ` display: grid; grid-template-columns: repeat( ${gridNumber ** 2}, 1fr);
+          grid-template-rows:repeat( ${gridNumber ** 2}, 1fr);`
 
     }
+    let gridCellsOnHover = drowingBoard.querySelectorAll('div');
+    gridCellsOnHover.forEach(gridCell => gridCell.addEventListener('mouseover', () => gridCell.style.backgroundColor = `${color}`));
 
 };
 // generating a random hexcode color, will invoked when randombtn is klicked
-let randomHexCode = 0;
-function getRandomColor() {
+
+function getRandomColor(color) {
     colors = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F");
     let randone = colors[Math.floor(Math.random() * colors.length)];
     let randtwo = colors[Math.floor(Math.random() * colors.length)];
@@ -45,27 +48,30 @@ function getRandomColor() {
     let randfour = colors[Math.floor(Math.random() * colors.length)];
     let randfive = colors[Math.floor(Math.random() * colors.length)];
     let randsix = colors[Math.floor(Math.random() * colors.length)];
-    randomHexCode = '#' + randone + randtwo + randthree + randfour + randfive + randsix;
-    return randomHexCode
+    randomColor = '#' + randone + randtwo + randthree + randfour + randfive + randsix;
+    return randomColor
 
 }
-let color = '#000000';
-console.log(randomHexCode);
+getRandomColor();
+
+console.log(randomColor);
 
 // setting grid celss to 16*16 as the defult
 creatGridCells(gridNumber);
-function colorGridCells() {
-    let gridCellsOnHover = document.querySelectorAll('gridCells');
-    gridCellsOnHover.forEach(gridCells => gridCells.style.cssText = `background-color:#000000;`);
-};
+
+//function colorGridCells() {
+
+//gridCellsOnHover.forEach(gridCells => gridCells.addEventListener('mouseover', clearButton))
+//};
 
 function clearButton() {
-    let containedCells = drowingBoard.querySelectorAll('.gridCells');
+    let containedCells = drowingBoard.querySelectorAll('div');
     containedCells.forEach(gridCells => gridCells.style.cssText = 'background-color: #ffffff;');
     console.log(containedCells)
 }
-clearButton();
+//clearButton();
 
 
 // 
-drowingBoard.addEventListener('mouseover', colorGridCells);
+//drowingBoard.addEventListener('mouseover', colorGridCells);
+
